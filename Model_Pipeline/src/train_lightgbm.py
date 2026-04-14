e488fb3b7e51cabeb931a3de0aaae5135102affa
Aaditya Krishna <158815451+kizamehoshigaki@users.noreply.github.com>
2026-04-14 04:08:53 -0400

    Add predictions by split to training output

--- Model_Pipeline/src/train_lightgbm.py
+++ Model_Pipeline/src/train_lightgbm.py
@@ -122,6 +122,7 @@ def train_single_horizon(train_df, val_df, test_df, horizon, params=LIGHTGBM_PAR
         "preds_by_split": {
             "val": (y_val, model.predict(X_val)),
             "test": (y_test, model.predict(X_test)),
+        },
     }
 
 
