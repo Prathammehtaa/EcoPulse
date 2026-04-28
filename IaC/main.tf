f1e0312a4f64a5f2b058f6cc046f4b4cd5fc066b
Pratham <prathammehta01712@gmail.com>
2026-04-28 00:58:38 -0400

    fix: add cloud-platform scope to GKE node pool for Artifact Registry access

--- IaC/main.tf
+++ IaC/main.tf
@@ -157,12 +157,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # Scopes matching the gcloud command
     oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append"
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
 
     # Disable legacy endpoints
